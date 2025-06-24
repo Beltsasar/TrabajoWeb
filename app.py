f7ca3ca28be03042be6972d75334c8161614dc0f
Daniel <da.plasencia@duocuc.cl>
2025-06-24 13:16:11 -0400

    modificando app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
-app = Flask(__name__, template_folder='Templates') 
+app = Flask(__name__)
+
 # Configuración de la base de datos
 # Render automáticamente inyecta la DATABASE_URL para PostgreSQL
 DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://user:password@localhost:5432/mydatabase')
